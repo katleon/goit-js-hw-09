0d1028302b5f7f56cd5ad16d3877944d9339b24c
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-03-05 17:23:26 +0100

    Created: task 1 and task 2
    Added: flatpickr and notiflix;
    Added: styles for links and task 2 values

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyBackground = document.querySelector('body');
+let timer = null;
 
+function onBtnDisabled() {
+  if (!startBtn.disabled) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  } else {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+}
+
+function getRandomHexColor() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return randomColor;
+}
+
+startBtn.addEventListener('click', () => {
+  onBtnDisabled();
+  timer = setInterval(() => {
+    bodyBackground.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  onBtnDisabled();
+});
